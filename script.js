23b648fd833aae86fa2ec893e527adf2099d87e0
samerbahri98 <samer.bahri@ieee.org>
2020-01-15 00:03:35 +0100

    changeable title feature

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-
-let newTask = document.querySelector("#taskText")
-let newTaskADD = document.querySelector("#addTask")
+//Global Declaraions
 let tasksList = document.querySelector("#tasksList")
 
-//remove task
+
+//Edit Title
+function setTitleChange(){
+    let editTitleText=document.querySelector("#Title")
+    editTitleText.addEventListener('keyup',function(e){
+        if(e.keyCode===13){
+            e.target.parentNode.innerHTML=`
+                <label id="titleLabel">${editTitleText.value}</label>
+                <span class="icon is-small">
+                    <i class="fa fa-pencil" id="editTitleBtn"></i>
+                </span> `
+                startTitleChange()
+        }
+    })
+}
+
+function startTitleChange(){
+    let editTitleBtn = document.querySelector("#editTitleBtn")
+    editTitleBtn.addEventListener('click',function(e){
+        if(e.target.id==="editTitleBtn")
+        {
+            e.target.parentNode.parentNode.innerHTML=`
+            <input class="input is-small" id="Title" type="text" placeholder="Title" width="150px">
+            `
+            setTitleChange()
+        }
+
+    })
+}
+startTitleChange()
+
+//Remove Task
 function setDelete(){
     let deleteButtons = document.querySelectorAll(".delete")
     deleteButtons.forEach(function(btn){
         btn.addEventListener('click',function(e){
             if(e.target.className==="delete")
             {
-                e.target.parentNode.parentNode.id="removed"
+                e.target.parentNode.parentNode.parentNode.id="removed"
                 removed=document.querySelector("#removed")
                 tasksList.removeChild(removed)
             }
@@ -20,30 +49,46 @@ function setDelete(){
 
 setDelete()
 
+//Add Task
+let newTask = document.querySelector("#taskText")
+let newTaskADD = document.querySelector("#addTask")
+addTask=function(e){
+        if(newTask.value!== "")
+        {
+            let task= document.createElement("div")
+            task.className="panel-block"
+            task.className="dropPanel"
+            task.innerHTML= `
+            <a class="panel-block dragPanel" draggable="true">
+            <span class="panel-icon">
+            <button class="delete"></button>
+            </span>
+            <h1>${newTask.value}</h1>
+            </a>`;
+            tasksList.appendChild(task) 
+            setDelete()
+            newTask.value=''
+        }
+}
 
-//add Task
 newTaskADD=addEventListener ('click',function(e){
-    if(e.target.id==="addTask")
-    {
-        let task= document.createElement("a")
-        task.className="panel-block"
-        task.innerHTML= `
-    <span class="panel-icon">
-        <button class="delete"></button>
-    </span>
-    <h1>${newTask.value}</h1>
-        `;
-        tasksList.appendChild(task) 
-        setDelete()
+    if(e.target.id==="addTask"){
+        addTask(e)
+    }
+})
+newTask.addEventListener('keyup',function(e){
+    if(e.keyCode===13){
+        addTask(e)
     }
 })
 
 
-
-//clear ALL
+//Clear ALL
 let clearAll=document.querySelector("#clearAll")
 clearAll.addEventListener('click',function(e){
     if(e.target.id==="clearAll"){
         tasksList.innerHTML=''
     }
 })
+
+
